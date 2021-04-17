381084c46e8602c4c60d69b91051cd8d70709383
Kenny <kenneththelarson@gmail.com>
2021-04-16 23:28:01 -0600

    contact form test

--- src/components/PhotoList/__tests__/index.test.js
+++ src/components/PhotoList/__tests__/index.test.js
@@ -2,17 +2,17 @@
 import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import PhotoList from '../'
+import PhotoList from './PhotoList'
 
 afterEach(cleanup)
 
 describe('PhotoList is rendering', () => {
-    it('renders', () => {
-        render(<PhotoList />);
-    });
+  it('renders', () => {
+    render(<PhotoList />);
+  });
 
-    it('renders', () => {
-        const { asFragment } = render(<PhotoList />)
-        expect(asFragment()).toMatchSnapshot()
-    });
+  it('renders', () => {
+    const { asFragment } = render(<PhotoList />)
+    expect(asFragment()).toMatchSnapshot()
+  });
 });
